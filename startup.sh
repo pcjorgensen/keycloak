f9919e2e7d0f4eee2f84a63a75b2e46d8ae7e7bb
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-12-16 14:21:47 -0700

    Delete startup.sh

--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PASSWORD=`cat /secret-volume/password`
-#/opt/keycloak-4.8.3.Final/bin/add-user-keycloak.sh --user admin --password $PASSWORD --realm master
-chmod 0700 /secret-volume

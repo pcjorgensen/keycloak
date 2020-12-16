1cc1b000c0200894b7d8a4efff4465dc3aebe7e1
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-12-16 14:13:08 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PASSWORD=`cat /secret-volume/password`
-/opt/keycloak-4.8.3.Final/bin/add-user-keycloak.sh --user admin --password $PASSWORD --realm master
+#/opt/keycloak-4.8.3.Final/bin/add-user-keycloak.sh --user admin --password $PASSWORD --realm master
 chmod 0700 /secret-volume

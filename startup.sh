2470ea56ea3538d0da18f595ba3851865217570d
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2020-12-18 13:09:03 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@ export PASSWORD=`cat /secret-volume/password` && touch test && echo $PASSWORD >>
 /opt/keycloak-4.8.3.Final/bin/jboss-cli.sh 'embed-server,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=proxy-address-forwarding,value=true)'
 /opt/keycloak-4.8.3.Final/bin/jboss-cli.sh 'embed-server,/socket-binding-group=standard-sockets/socket-binding=proxy-https:add(port=8443)'
 /opt/keycloak-4.8.3.Final/bin/jboss-cli.sh 'embed-server,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=redirect-socket,value=proxy-https)'
-chmod 0700 /secret-volume
+chmod 0700 /secret-volume && chmod 0700 /secret-volume/password
 supervisorctl restart apache

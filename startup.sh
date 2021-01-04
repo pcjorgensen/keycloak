a7faa1e3c6a5ea8c86ee8fafe32221377e18e532
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-01-04 14:40:51 -0700

    trying to manually start apache

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ export PASSWORD=`cat /secret-volume/password`
 /opt/keycloak-4.8.3.Final/bin/jboss-cli.sh 'embed-server,/socket-binding-group=standard-sockets/socket-binding=proxy-https:add(port=8443)'
 /opt/keycloak-4.8.3.Final/bin/jboss-cli.sh 'embed-server,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=redirect-socket,value=proxy-https)'
 chmod 0700 /secret-volume && chmod 0700 /secret-volume/password
-supervisorctl restart apache
+httpd -D "FOREGROUND" -k start

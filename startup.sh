ff12ff0a344103b8e4abe8c437e360020cd9171f
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-01-04 13:10:05 -0700

    trying again

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export PASSWORD=`cat /secret-volume/password` && /opt/keycloak-4.8.3.Final/bin/add-user-keycloak.sh --user admin --password $PASSWORD --realm master
+/opt/keycloak-4.8.3.Final/bin/jboss-cli.sh 'embed-server,/socket-binding-group=standard-sockets/socket-binding=proxy-https:add(port=8443)'
 chmod 0700 /secret-volume && chmod 0700 /secret-volume/password
 supervisorctl restart apache

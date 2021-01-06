c789065449f3dd64ed057ddf99072f6dd1a6e2c4
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-01-06 12:29:34 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,4 @@
 sleep 10
 export PASSWORD=`cat /secret-volume/password`
 /opt/keycloak-4.8.3.Final/bin/add-user-keycloak.sh --user admin --password $PASSWORD --realm master
-/opt/keycloak-4.8.3.Final/bin/jboss-cli.sh 'embed-server,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=proxy-address-forwarding,value=true)'
-/opt/keycloak-4.8.3.Final/bin/jboss-cli.sh 'embed-server,/socket-binding-group=standard-sockets/socket-binding=proxy-https:add(port=8443)'
-/opt/keycloak-4.8.3.Final/bin/jboss-cli.sh 'embed-server,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=redirect-socket,value=proxy-https)'
 chmod 0700 /secret-volume && chmod 0700 /secret-volume/password
